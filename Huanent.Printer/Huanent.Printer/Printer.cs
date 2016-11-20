25e197728f60cb93d5396a01d4ceef366eee7f44
李凤焕 <huanent@hotmail.com>
2016-11-20 21:07:16 +0800

    搭建基本结构

---
+++ Huanent.Printer/Huanent.Printer/Printer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huanent.Printer
+{
+    class Printer : IPrinter
+    {
+        int _paperWidth;
+        PrintQueue _printer;
+        int heigthOffset=0;
+        internal Printer(int paperWidth, PrintQueue printer)
+        {
+            _paperWidth = paperWidth;
+            _printer = printer;
+        }
+
+        public void FeedLine(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintCenterText(string content, int size = 10)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintLine(char c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintOffsetText(string content, int size = 10, int Offset = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintRightOffsetText(string content, int size = 10, int offset = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintRightText(string content, int size = 10)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintText(string content, int size = 10)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
